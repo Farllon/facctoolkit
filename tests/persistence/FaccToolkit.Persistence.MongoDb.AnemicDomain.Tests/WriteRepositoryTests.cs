e2b3f553a25d4a52c5ee1cf060bff7ef1a470dda
Farllon Costa <farllon.costa@outlook.com>
2024-09-01 08:08:24 -0300

    Adiciona os testes do FaccToolkit.Persistence.MongoDb.AnemicDomain

---
+++ tests/persistence/FaccToolkit.Persistence.MongoDb.AnemicDomain.Tests/WriteRepositoryTests.cs
@@ -0,0 +1,175 @@
+﻿using FaccToolkit.Persistence.MongoDb.Abstractions;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Moq;
+using System.Linq.Expressions;
+
+namespace FaccToolkit.Persistence.MongoDb.AnemicDomain.Tests
+{
+    public class WriteRepositoryTests
+    {
+        [Fact]
+        public async Task WhenInsert_ShouldCallModelRepositoryInsert()
+        {
+            var contextMock = new Mock<IMongoDbContext>();
+            var collectionMock = Mock.Of<IMongoCollection<TestEntity>>();
+            var modelRepositoryMock = new Mock<IModelRepository<TestEntity>>();
+            var entity = new TestEntity();
+
+            contextMock
+                .Setup(context => context.GetCollection<TestEntity, Guid>(It.IsAny<string>()))
+                .Returns(collectionMock);
+
+            contextMock
+                .Setup(context => context.GetModelRepository<TestEntity>(
+                    It.IsAny<string>(),
+                    It.IsAny<ILogger>()))
+                .Returns(modelRepositoryMock.Object);
+
+            modelRepositoryMock
+                .Setup(modelRepository => modelRepository.InsertAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entity,
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            var repository = new WriteRepository<TestEntity, Guid>(
+                "test-collection",
+                contextMock.Object,
+                Mock.Of<ILogger<WriteRepository<TestEntity, Guid>>>());
+
+            await repository.InsertAsync(entity, CancellationToken.None);
+
+            modelRepositoryMock.Verify(
+                modelRepository => modelRepository.InsertAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entity,
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task WhenInsertMany_ShouldCallModelRepositoryInsertMany()
+        {
+            var contextMock = new Mock<IMongoDbContext>();
+            var collectionMock = Mock.Of<IMongoCollection<TestEntity>>();
+            var modelRepositoryMock = new Mock<IModelRepository<TestEntity>>();
+            var entity = new TestEntity();
+            var entities = new List<TestEntity>
+            {
+                entity
+            };
+
+            contextMock
+                .Setup(context => context.GetCollection<TestEntity, Guid>(It.IsAny<string>()))
+                .Returns(collectionMock);
+
+            contextMock
+                .Setup(context => context.GetModelRepository<TestEntity>(
+                    It.IsAny<string>(),
+                    It.IsAny<ILogger>()))
+                .Returns(modelRepositoryMock.Object);
+
+            modelRepositoryMock
+                .Setup(modelRepository => modelRepository.InsertAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entities,
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            var repository = new WriteRepository<TestEntity, Guid>(
+                "test-collection",
+                contextMock.Object,
+                Mock.Of<ILogger<WriteRepository<TestEntity, Guid>>>());
+
+            await repository.InsertAsync(entities, CancellationToken.None);
+
+            modelRepositoryMock.Verify(
+                modelRepository => modelRepository.InsertAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entities,
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task WhenUpdate_ShouldCallModelRepositoryUpdate()
+        {
+            var contextMock = new Mock<IMongoDbContext>();
+            var collectionMock = Mock.Of<IMongoCollection<TestEntity>>();
+            var modelRepositoryMock = new Mock<IModelRepository<TestEntity>>();
+            var entity = new TestEntity();
+
+            contextMock
+                .Setup(context => context.GetCollection<TestEntity, Guid>(It.IsAny<string>()))
+                .Returns(collectionMock);
+
+            contextMock
+                .Setup(context => context.GetModelRepository<TestEntity>(
+                    It.IsAny<string>(),
+                    It.IsAny<ILogger>()))
+                .Returns(modelRepositoryMock.Object);
+
+            modelRepositoryMock
+                .Setup(modelRepository => modelRepository.UpdateAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entity,
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<TestEntity?>(entity));
+
+            var repository = new WriteRepository<TestEntity, Guid>(
+                "test-collection",
+                contextMock.Object,
+                Mock.Of<ILogger<WriteRepository<TestEntity, Guid>>>());
+
+            await repository.UpdateAsync(entity, CancellationToken.None);
+
+            modelRepositoryMock.Verify(
+                modelRepository => modelRepository.UpdateAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entity,
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task WhenDelete_ShouldCallModelRepositoryDelete()
+        {
+            var contextMock = new Mock<IMongoDbContext>();
+            var collectionMock = Mock.Of<IMongoCollection<TestEntity>>();
+            var modelRepositoryMock = new Mock<IModelRepository<TestEntity>>();
+            var entity = new TestEntity();
+
+            contextMock
+                .Setup(context => context.GetCollection<TestEntity, Guid>(It.IsAny<string>()))
+                .Returns(collectionMock);
+
+            contextMock
+                .Setup(context => context.GetModelRepository<TestEntity>(
+                    It.IsAny<string>(),
+                    It.IsAny<ILogger>()))
+                .Returns(modelRepositoryMock.Object);
+
+            modelRepositoryMock
+                .Setup(modelRepository => modelRepository.DeleteAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entity.Id,
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<TestEntity?>(entity));
+
+            var repository = new WriteRepository<TestEntity, Guid>(
+                "test-collection",
+                contextMock.Object,
+                Mock.Of<ILogger<WriteRepository<TestEntity, Guid>>>());
+
+            await repository.DeleteAsync(entity.Id, CancellationToken.None);
+
+            modelRepositoryMock.Verify(
+                modelRepository => modelRepository.DeleteAsync(
+                    It.IsAny<Expression<Func<TestEntity, Guid>>>(),
+                    entity.Id,
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+    }
+}
